49c56922ff7ea4b081992ab58937cb4042059a69
Miklos Vajna <vmiklos@frugalware.org>
2008-04-06 21:29:01 +0200

    cosmetics for later commands

--- lpbot.c
+++ lpbot.c
@@ -181,9 +181,13 @@ int lp_handler(GIOChannel *source, GIOCondition condition, gpointer data)
 	}
 	else if(!strcmp(msg->cmd, "PRIVMSG"))
 	{
-		if(!strncmp(server->nick, g_list_nth_data(msg->params, 0), strlen(server->nick)) &&
-				!strcmp("ping", g_list_nth_data(msg->params, 1)))
-			lp_send(server, "privmsg %s :pong", msg->to);
+		if(!strncmp(server->nick, g_list_nth_data(msg->params, 0), strlen(server->nick)))
+		{
+			// FIXME: this could be more generic to provide
+			// help, etc
+			if(!strcmp("ping", g_list_nth_data(msg->params, 1)))
+					lp_send(server, "privmsg %s :pong", msg->to);
+		}
 	}
 	lp_msg_free(msg);
 	return TRUE;
